372c19823bb5da604fed3687cf683ba7cc7444c5
Shayanta Shopnil <shopnil710@gmail.com>
2024-12-25 03:08:53 +0600

    expense ui init

--- frontend/src/pages/Expense/index.jsx
+++ frontend/src/pages/Expense/index.jsx
@@ -1,7 +1,22 @@
-export const Expense = () =>{
-    return(
-        <div>
-            Expense
+import { ExpenseRecord } from "./components/AddExpense"
+import ECalander from "./components/ECalander"
+import './expense.css'
+export const Expense = () => {
+    return (
+        <div 
+            style={{
+                display:'flex',
+                flexDirection:'column',
+                justifyContent:'center'
+            }}        
+        >
+            <ExpenseRecord />
+            <div className="transactionSection">
+            <div className="calander-bg">
+                <ECalander/>
+            </div>
+            
+            </div>
         </div>
     )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/x-date-pickers/LocalizationProvider': '@mui/x-date-pickers',
+    },
   server: {
     watch: {
       usePolling: true
     }
   },
-})
+  
+}})
