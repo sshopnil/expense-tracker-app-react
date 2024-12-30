30e283358ba5ec2fcc415e8235df0e78654b615a
Shayanta Shopnil <shopnil710@gmail.com>
2024-12-31 04:29:10 +0600

    implemented arima model to forecast data

--- backend/routes/transactions.route.js
+++ backend/routes/transactions.route.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 //controllers
 const {get_all_expenses, add_expense, add_fund, get_fund} = require('../services/transactions.services');
-
+const {arima_forecast} = require('../services/forecasting.services');
 
 //routes
 router.post('/add-expense/:user_id', add_expense);
 router.post('/add-fund/:user_id', add_fund);
 router.get('/fund/:user_id', get_fund);
 router.get('/expense/:user_id', get_all_expenses);
-// router.get('/forecast');
+router.get('/forecast/:user_id', arima_forecast);
 module.exports = router;
---
+++ backend/services/forecasting.services.js
@@ -0,0 +1,26 @@
+const ARIMAPromise = require('arima/async');
+const ExpenseSchema = require('../schemas/transactions.expense-schema');
+
+
+exports.arima_forecast = async (req, res, next)=> {
+    const {user_id} = req.params;
+    const expenses = await ExpenseSchema.find({userId: user_id});
+    try {
+      const ARIMA = await ARIMAPromise; // Wait for ARIMA to be loaded
+      const ts = expenses.map((item=> item.amount));
+
+    //   const ts = data.map((item)=>item.amount);
+      const arima = new ARIMA({ p: 0, d: 1, q: 10, P: 0, D: 0, Q: 0, S: 0, verbose: false }).train(ts);
+      const [pred, errors] = arima.predict(7);
+  
+      console.log(pred); // Predicted values
+      // You can also log errors or return the predictions if needed
+      res.json(pred);
+    } catch (error) {
+      console.error('Error during ARIMA prediction:', error);
+    }
+    finally{
+        next();
+    }
+  }
+  
