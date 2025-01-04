5112187986fe95aa77e73f7f3ed4db31a0fb11ad
Shayanta Shopnil <shopnil710@gmail.com>
2025-01-04 21:19:54 +0600

    arima with date and value

--- backend/services/forecasting.services.js
+++ backend/services/forecasting.services.js
@@ -1,28 +1,49 @@
 const ARIMAPromise = require('arima/async');
 const ExpenseSchema = require('../schemas/transactions.expense-schema');
+const dayjs = require('dayjs');
 
+exports.arima_forecast = async (req, res, next) => {
+  // #swagger.tags = ['/forecast']
 
-exports.arima_forecast = async (req, res, next)=> {
-    // #swagger.tags = ['/forecast']
+  const { user_id } = req.params;
+  const expenses = await ExpenseSchema.find({ userId: user_id }).sort({ date: 1 });
+  try {
+    const ARIMA = await ARIMAPromise;
+    const ts = expenses.map((item) => item.amount); 
 
-    const {user_id} = req.params;
-    const expenses = await ExpenseSchema.find({userId: user_id});
-    try {
-      const ARIMA = await ARIMAPromise; // Wait for ARIMA to be loaded
-      const ts = expenses.map((item=> item.amount));
+    const arima = new ARIMA({
+      p: 2,
+      d: 0,
+      q: 2,
+      P: 1,
+      D: 0,
+      Q: 1,
+      S: 2,
+      verbose: false
+    }).train(ts);
 
-    //   const ts = data.map((item)=>item.amount);
-      const arima = new ARIMA({ p: 0, d: 1, q: 10, P: 0, D: 0, Q: 0, S: 0, verbose: false }).train(ts);
-      const [pred, errors] = arima.predict(7);
-  
-      console.log(pred); // Predicted values
-      // You can also log errors or return the predictions if needed
-      res.json(pred);
-    } catch (error) {
-      console.error('Error during ARIMA prediction:', error);
+    const [pred_values, errors] = arima.predict(7); 
+
+    const latest_date = new Date(expenses[expenses.length - 1].date);
+
+    const future_dates = [];
+    for (let i = 1; i <= 7; i++) {
+      const future_date = new Date(latest_date);
+      future_date.setDate(latest_date.getDate() + i);
+      future_dates.push(dayjs(future_date).format('DD/MM/YYYY'));
     }
-    finally{
-        next();
-    }
+
+
+    const predicted_date_amount = future_dates.map((date, index) => ({
+      date: date,
+      amount: pred_values[index].toFixed(2)
+    }));
+
+    res.json(predicted_date_amount); 
+  } catch (error) {
+    console.error('Error during ARIMA prediction:', error);
+    res.status(500).json({ message: 'Error during ARIMA prediction' });
+  } finally {
+    next();
   }
-  
+};
